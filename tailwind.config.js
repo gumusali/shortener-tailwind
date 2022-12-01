d29d0a3bd224af783b4889a35eac3c018593d76a
Alihan Gümüş <gumusali03@gmail.com>
2022-12-02 01:01:15 +0300

    home page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '360px',
+      'md': '768px',
+      'lg': '1200px',
+    }
+  },
+  plugins: [],
+}
